a58418765b55aa938e224a7eb10419b90c1eeb4d
Galilea <sandovalgalilea@gmail.com>
2023-08-28 18:52:43 -0700

    tareaviernes

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-hola soy ale
-=======
-hola soy ana lucia 
 
-nirvana estuvo aqui
->>>>>>> a014be0839cfe8fe9f086daa0a644c36773637a4
+#hola soy ale
+#>>>>hola soy ana lucia 
+
+#>>>>> nirvana estuvo aqui
+#>>>>>>> a014be0839cfe8fe9f086daa0a644c36773637a4
